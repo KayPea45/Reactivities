a15d85b339a610ce709d48c730af6c7fe922d5cb
Kelvin P <95223371+KayPea45@users.noreply.github.com>
2025-03-14 23:01:29 +1100

    UI changes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,30 +1,28 @@
 import NavBar from "./NavBar";
-// import { Outlet, useLocation } from "react-router-dom";
-// import HomePage from "../../features/home/HomePage";
-// import agent from "../api/agent";
 import { Box, Container, CssBaseline } from "@mui/material";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 
 // NOTE: React query handles state management on the server side. Mobx handles state management on the client side.
 function App() {
+	const location = useLocation();
 	return (
-		<>
-			{/* {location.pathname === "/" ? (
+		<Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
+			<CssBaseline />
+			{location.pathname === "/" ? (
 				<HomePage />
-			) : ( */}
-			<Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
-				<CssBaseline />
-				<NavBar />
-				<Container /*className="main-list"*/ maxWidth="xl" sx={{ mt: 3 }}>
-					{/* 
-					The parent route defines a layout or structure that will be shared by all its child routes.
-						 The Outlet component is placed in the parent component where the child components should be rendered.  */
-					}
-					<Outlet />
-				</Container>
-			</Box>
-			{/* )} */}
-		</>
+			) : (
+				<>
+					<NavBar />
+					<Container /*className="main-list"*/ maxWidth="xl" sx={{ mt: 3 }}>
+						{/*
+						The parent route defines a layout or structure that will be shared by all its child routes.
+						The Outlet component is placed in the parent component where the child components should be rendered.  */}
+						<Outlet />
+					</Container>
+				</>
+			)}
+		</Box>
 	);
 }
 
